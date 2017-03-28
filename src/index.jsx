be3b294e1eeca822e784598b417ee706d7a14f1a
Andrew Lee <andrew@andrewksl.com>
2017-03-28 17:49:55 +0800

    Add route for Andrew's Page

--- src/index.jsx
+++ src/index.jsx
@@ -4,19 +4,21 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import App from './app/App';
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFoundPage'
+import AndrewsPage from './pages/AndrewsPage'
 import 'skeleton-css/css/normalize.css'
 import 'skeleton-css/css/skeleton.css'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-
   ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <Route path="andrew" component={AndrewsPage} />
         <IndexRoute component={HomePage} />
         <Route path="*" component={NotFoundPage}/>
       </Route>
     </Router>,
     document.getElementById('root')
   );
+
 });
