a63a9c0f549155e20837f1b95ae3761857239a21
Andrew Lee <andrew@andrewksl.com>
2017-03-28 17:57:04 +0800

    Add link on home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import {Link} from 'react-router'
 
 export default class HomePage extends Component {
 
@@ -15,9 +16,39 @@ export default class HomePage extends Component {
             <h1>Welcome.</h1>
             Welcome to the Jetspree exercise.
 
+            <PageLinkContainer>
+              <PageLink name="Andrew" path="/andrew"/>
+            </PageLinkContainer>
+
           </div>
         </div>
       </div>
     );
   }
 }
+
+class PageLinkContainer extends Component {
+
+  render() {
+    return (
+      <div className="PageLinkContainer">
+        {this.props.children}
+      </div>
+    );
+  }
+
+}
+
+class PageLink extends Component {
+
+  render() {
+
+    return (
+      <div className="PageLink">
+        <h4>{this.props.name}</h4>
+        <Link to={this.props.path}>Click here</Link>
+      </div>
+    );
+  }
+}
+
