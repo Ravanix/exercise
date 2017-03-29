d09e72b172bea7cec3877456d96bfc9dfe32b474
blackyu321 <yuho.chiu@jetspree.com>
2017-03-29 12:13:37 +0800

    no message

--- src/components/TopNavigation.jsx
+++ src/components/TopNavigation.jsx
@@ -8,7 +8,7 @@ export default class TopNavigation extends Component {
     return (
       <ul id="TopNavigation">
         <NavItem path="/">Home</NavItem>
-        <NavItem path="/andrew">Andrew</NavItem>
+        <NavItem path="/andrew">Andrew XX</NavItem>
       </ul>
     );
   }
